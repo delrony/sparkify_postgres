abb9eded2c4fbcff6d90e79eb64e15287e4aa7a7
Muhammed Najmul Huda <del.rony@yahoo.com>
2020-12-23 10:32:23 +0000

    Add NOT NULL constraints, use ON CONFLICT DO UPDATE in users table

--- sql_queries.py
+++ sql_queries.py
@@ -9,23 +9,59 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 # CREATE TABLES
 
 songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays
-(songplay_id SERIAL PRIMARY KEY, start_time bigint, user_id int, level varchar, song_id varchar, artist_id varchar, session_id int, location varchar, user_agent varchar)
+(
+    songplay_id SERIAL PRIMARY KEY, 
+    start_time bigint, 
+    user_id int NOT NULL, 
+    level varchar NOT NULL, 
+    song_id varchar, 
+    artist_id varchar, 
+    session_id int NOT NULL, 
+    location varchar, 
+    user_agent varchar
+)
 """)
 
 user_table_create = ("""CREATE TABLE IF NOT EXISTS users
-(user_id int PRIMARY KEY, first_name varchar, last_name varchar, gender varchar, level varchar)
+(
+    user_id int PRIMARY KEY, 
+    first_name varchar, 
+    last_name varchar, 
+    gender varchar, 
+    level varchar NOT NULL
+)
 """)
 
 song_table_create = ("""CREATE TABLE IF NOT EXISTS songs
-(song_id varchar PRIMARY KEY, title varchar, artist_id varchar, year int, duration float8)
+(
+    song_id varchar PRIMARY KEY, 
+    title varchar NOT NULL, 
+    artist_id varchar NOT NULL, 
+    year int, 
+    duration float8 NOT NULL
+)
 """)
 
 artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists
-(artist_id varchar PRIMARY KEY, name varchar, location varchar, latitude float8, longitude float8)
+(
+    artist_id varchar PRIMARY KEY, 
+    name varchar NOT NULL, 
+    location varchar, 
+    latitude float8, 
+    longitude float8
+)
 """)
 
 time_table_create = ("""CREATE TABLE IF NOT EXISTS time
-(start_time bigint PRIMARY KEY, hour smallint, day smallint, week smallint, month smallint, year smallint, weekday varchar)
+(
+    start_time bigint PRIMARY KEY, 
+    hour smallint, 
+    day smallint, 
+    week smallint, 
+    month smallint, 
+    year smallint, 
+    weekday varchar
+)
 """)
 
 # INSERT RECORDS
@@ -38,26 +74,26 @@ VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 user_table_insert = ("""INSERT INTO users
 (user_id, first_name, last_name, gender, level)
 VALUES (%s, %s, %s, %s, %s)
-ON CONFLICT DO NOTHING
+ON CONFLICT (user_id) DO UPDATE SET level = EXCLUDED.level
 """)
 
 song_table_insert = ("""INSERT INTO songs
 (song_id, title, artist_id, year, duration)
 VALUES (%s, %s, %s, %s, %s)
-ON CONFLICT DO NOTHING
+ON CONFLICT (song_id) DO NOTHING
 """)
 
 artist_table_insert = ("""INSERT INTO artists
 (artist_id, name, location, latitude, longitude)
 VALUES (%s, %s, %s, %s, %s)
-ON CONFLICT DO NOTHING
+ON CONFLICT (artist_id) DO NOTHING
 """)
 
 
 time_table_insert = ("""INSERT INTO time
 (start_time, hour, day, week, month, year, weekday)
 VALUES (%s, %s, %s, %s, %s, %s,  %s)
-ON CONFLICT DO NOTHING
+ON CONFLICT (start_time) DO NOTHING
 """)
 
 # FIND SONGS
